36e3fc8145c5accde28c92231a19d8ce5d26f604
iguemanuel <igor17emanuel@gmail.com>
2024-04-23 20:13:34 -0300

    Logo SVG e Tailwind Instalado

---
+++ front/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/components/home/home.component.html" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
