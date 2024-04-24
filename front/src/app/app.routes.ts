90b8de0479c374cf05652136f11b77db3759c0b2
nestorhugo <nestorhugo@proton.me>
2024-04-23 21:45:15 -0300

    adicionado tela de base do login

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 export const routes: Routes = [
-    {   
-        path: "",
-        component: HomeComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
 ];
